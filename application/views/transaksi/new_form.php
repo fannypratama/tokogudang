84b24c18f24daca8d3d48aeab276b0bc0b050d54
fannypratama <arsemfanny4@gmail.com>
2019-11-22 11:05:15 +0700

    up

--- application/views/transaksi/new_form.php
+++ application/views/transaksi/new_form.php
@@ -39,42 +39,46 @@ $hasil_10 = acakangka(1);
 
 ?>
 <div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="row bg-title">
-            <div class="">
-                <a href="<?php echo site_url('transaksi') ?>"><i class="fa fa-arrow-left"></i> Back</a>
-            </div>
-
-
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="white-box">
-                    <div class="row">
-                        <div class="col-sm-12 col-xs-12">
-                            <?php if ($this->session->flashdata('success')) : ?>
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        
+                    </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                        <ol class="breadcrumb">
+                            <li><a href="<?php echo site_url('transaksi') ?>">Transaksi</a></li>
+                            <li class="active">Add Transaksi</a></li>
+                        </ol>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+         <div class="col-sm-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"><a href="<?php echo site_url('transaksi') ?>"><i class="fa fa-arrow-left"></i> Back </a> 
+                                <div class="panel-action"><a href="#" data-perform="panel-collapse"><i class="ti-minus"></i></a> <a href="#" data-perform="panel-dismiss"><i class="ti-close"></i></a></div>
+                            </div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+<br>                            <?php if ($this->session->flashdata('success')) : ?>
                                 <div class="alert alert-success" role="alert">
                                     <?php echo $this->session->flashdata('success'); ?>
                                 </div>
                             <?php endif; ?>
-                            <form action="<?php base_url('supplier/add') ?>" method="post" enctype="multipart/form-data">
+                            <form action="<?php base_url('supplier/add') ?>" method="post" enctype="multipart/form-data"class="form-horizontal">
                                 <input class="form-control <?php echo form_error('kode_supplier') ? 'is-invalid' : '' ?>" type="hidden" name="kode_supplier" placeholder="Kode Supplier" value="<?= $hasil_1, $hasil_2, $hasil_3, $hasil_4, $hasil_5, $hasil_6, $hasil_7, $hasil_8, $hasil_9, $hasil_10; ?>" />
 
-                                <div class="form-group">
-                                    <label for="nama_barang">Nama Barang</label>
-                                    <div class="input-group">
-                                        <div class="input-group-addon"><i class="ti-user"></i></div>
-                                        <input class="form-control <?php echo form_error('nama_barang') ? 'is-invalid' : '' ?>" type="text" name="nama_barang" placeholder="Nama Barang" />
+                                <div class="form-group row">
+                                    <label class="col-sm-3 control-label col-form-label">Nama Barang</label>
+                                    <div class="col-sm-7">        <input class="form-control <?php echo form_error('nama_barang') ? 'is-invalid' : '' ?>" type="text" name="nama_barang" placeholder="Nama Barang" />
                                     </div>
                                     <div class="invalid-feedback">
                                         <?php echo form_error('nama_barang') ?>
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <label for="nama">Nilai System</label>
-                                    <div class="input-group">
-                                        <div class="input-group-addon"><i class="ti-user"></i></div>
+                               <div class="form-group row">
+                                    <label class="col-sm-3 control-label col-form-label">Nilai System</label>
+                                    <div class="col-sm-7">
                                         <input class="form-control <?php echo form_error('nilai') ? 'is-invalid' : '' ?>" type="number" name="nilai" placeholder="Nilai" />
                                     </div>
                                     <div class="invalid-feedback">
@@ -83,7 +87,8 @@ $hasil_10 = acakangka(1);
                                 </div>
                                 
 
-
+                                 <div class="form-group m-b-0">
+                                    <div class="offset-sm-3 col-sm-7">
                                 <button type="submit" class="btn btn-success waves-effect waves-light m-r-10" name="btn" value="Save">Submit</button>
                                 <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
                             </form>
