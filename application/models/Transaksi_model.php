4ffd9e782218bda17899912be6f7ee0f755c7efe
fannypratama <arsemfanny4@gmail.com>
2019-11-22 11:05:47 +0700

    Update Transaksi_model.php

--- application/models/Transaksi_model.php
+++ application/models/Transaksi_model.php
@@ -1,17 +1,11 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
-
 class Transaksi_model extends CI_Model
 {
     private $_table = "ts";
-
     public $id_ts;
     public $nama_barang;
     public $nilai;
    
-
-
-
-
     public function rules()
     {
         return [
@@ -20,44 +14,35 @@ class Transaksi_model extends CI_Model
                 'label' => 'Nilai',
                 'rules' => 'required'
             ]
-
         ];
     }
-
     public function getAll()
     {
         return $this->db->get($this->_table)->result();
     }
-
     public function getById($id)
     {
         return $this->db->get_where($this->_table, ["id_ts" => $id])->row();
     }
-
-
-
     public function save()
     {
         $post = $this->input->post();
-        $this->nama_barang = $post['nama_barang'];
-        $transaksi = $this->db->get_where($this->_table, ['nama_barang' => $this->nama_barang])->result();
+        $data['nama_barang'] = $post['nama_barang'];
+        $data['nilai'] = $post['nilai'];
+        $transaksi = $this->db->get_where($this->_table, ['nama_barang' => $data['nama_barang']])->row_array();
         if ($transaksi == null) {
-            $this->nilai = $post['nilai'];
-            $this->nama_barang = $post['nama_barang'];
-            $this->db->insert($this->_table, $this);
+            $this->db->insert($this->_table, $data);
         } else {
-            var_dump($transaksi->nilai);
-            // $angka = 
-            // $transaksi->nilai +
-            // $post['nilai'];
-
-            // $this->db->update($this->_table, $langkah, array('nama_barang' => $post['nama_barang']));
+            var_dump($transaksi['nilai']);
+            $angka = 
+            $transaksi['nilai'] +
+            $post['nilai'];
+            $nilai = [
+                'nilai' => $angka
+            ];
+            $this->db->update($this->_table, $nilai, ['nama_barang' => $post['nama_barang']]);
         }
-
     }
-
-
-
     public function update()
     {
         $post = $this->input->post();
@@ -67,24 +52,19 @@ class Transaksi_model extends CI_Model
         $this->satuan = $post["satuan"];
         $this->uraian = $post["uraian"];
         $this->nama_kategori = $post["nama_kategori"];
-
         if (!empty($_FILES["image"]["name"])) {
             $this->image = $this->_uploadImage();
         } else {
             $this->image = $post["old_image"];
         }
-
         $this->db->update($this->_table, $this, array('id_mbarang' => $post['id']));
     }
-
     public function delete($id)
     {
         return $this->db->delete($this->_table, array("id_mbarang" => $id));
     }
-
     private function _uploadImage()
     {
-
         $config['upload_path']          = './upload/mbarang';
         $config['allowed_types']        = 'jpg|png|gif';
         $config['file_name']            = $this->id_mbarang;
@@ -92,14 +72,12 @@ class Transaksi_model extends CI_Model
         $config['max_size']             = 10240; // 1MB
         // $config['max_width']            = 1024;
         // $config['max_height']           = 768;
-
         $this->load->library('upload', $config);
         $this->upload->initialize($config);
         if ($this->upload->do_upload('foto')) {
             return $this->upload->data("file_name");
         }
         print_r($this->upload->display_errors());
-
         return "default.jpg";
     }
 }
