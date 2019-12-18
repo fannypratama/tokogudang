19714a77dda9079d60051e359519246d213709da
rio <wakwaubahagia4@gmail.com>
2019-12-18 10:52:36 +0700

    Chart (kurang perbaikan tampilan)

---
+++ application/views/chatBaru.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Chart using codeigniter and morris.js</title>
+    <link rel="stylesheet" href="<?php echo base_url().'assets/js/morris.js-0.5.1/morris.css'?>">
+  </head>
+  <body>
+    <h2>Chart using Codeigniter and Morris.js</h2>
+ 
+    <div id="graph"></div>
+ 
+    <script src="<?= base_url('assets/'); ?>plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo base_url().'assets/js/raphael-min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/js/morris.js-0.5.1/morris.min.js'?>"></script>
+    <script>
+        Morris.Bar({
+          element: 'graph',
+          data: <?php echo $data;?>,
+          xkey: 'bulan',
+          ykeys: ['barang_masuk', 'barang_keluar'],
+          labels: ['Barang Masuk', 'Barang Keluar']
+        });
+    </script>
+  </body>
+</html>
